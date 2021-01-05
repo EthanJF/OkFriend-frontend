63699848e7552e533aff2bc27dd789a1c6984ec2
EthanJF <ethanjosephfournier@gmail.com>
2021-01-05 11:23:55 -0500

    fixed scroll in friends chat panel

--- src/components/FriendsChatPanel.js
+++ src/components/FriendsChatPanel.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react'
 import Friends from './Friends'
 import Chat from './Chat'
+import ReactDOM from 'react-dom'
 
 export default class FriendsChatPanel extends Component {
+    componentWillUpdate() {
+        const node = ReactDOM.findDOMNode(this)
+        this.shouldScrollToBottom = node.scrollTop + node.clientHeight + 100 >= node.scrollHeight
+    }
+    
+    componentDidUpdate() {
+        if (this.shouldScrollToBottom) {
+            const node = ReactDOM.findDOMNode(this)
+            node.scrollTop = node.scrollHeight   
+        }
+    }
     render(){
         const renderChats = this.props.allChats.map((chat) => {
             return <div onClick={() => this.props.startChatFromLI(chat)}><li>{chat.user1.username===this.props.username ? chat.user2.username : chat.user1.username}  <button onClick={() => this.props.deleteAChat(chat.id)}>Delete</button></li></div>
         })
         return(
-            <div className="friends-chat-panel">
+            <div className="friends-chat-panel" ref="friendsChatPanel">
                 <Friends friends={this.props.friends} userID={this.props.userID} setID={this.props.setID}/>
                 <h1>My Chats</h1>
                 <ul className="chat-list">
