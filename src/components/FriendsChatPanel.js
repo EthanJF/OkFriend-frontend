83a36110bad5881a107bb2c3d27464c11a78c1c6
EthanJF <ethanjosephfournier@gmail.com>
2021-01-06 11:20:24 -0500

    added scroll to top function for chat and misc padding fixes

--- src/components/FriendsChatPanel.js
+++ src/components/FriendsChatPanel.js
@@ -5,6 +5,10 @@ import ReactDOM from 'react-dom'
 
 export default class FriendsChatPanel extends Component {
 
+    state = {
+        showTopButton: false
+    }
+
     componentDidMount(){
         const node = ReactDOM.findDOMNode(this)
         this.shouldScrollToBottom = node.scrollTop + node.clientHeight + 100 >= node.scrollHeight
@@ -20,18 +24,40 @@ export default class FriendsChatPanel extends Component {
             node.scrollTop = node.scrollHeight   
         }
     }
+
+    scrollToTop = () => {
+        const node = ReactDOM.findDOMNode(this)
+        node.scrollTop = 0
+    }
+
+    scrollFunction = () => {
+        const node = ReactDOM.findDOMNode(this)
+        if (node.scrollTop > 20) {
+            this.setState ({
+                showTopButton: true
+            })
+        } else {
+            this.setState ({
+                showTopButton: false
+            })
+        }
+    }
+
+
+
     render(){
         const renderChats = this.props.allChats.map((chat) => {
             return <div onClick={() => this.props.startChatFromLI(chat)}><li>{chat.user1.username===this.props.username ? chat.user2.username : chat.user1.username}  <button onClick={() => this.props.deleteAChat(chat.id)}>Delete</button></li></div>
         })
         return(
-            <div className="friends-chat-panel" ref="friendsChatPanel">
+            <div className="friends-chat-panel" ref="friendsChatPanel" onScroll={this.scrollFunction}>
                 <Friends friends={this.props.friends} userID={this.props.userID} setID={this.props.setID}/>
                 <h1>My Chats</h1>
                 <ul className="chat-list">
                  {renderChats.length !== 0 ? renderChats : "You don't have any chats!"}
                 </ul>
                 <Chat username={this.props.username} selectedUserID={this.props.selectedUserID} userID={this.props.userID} allChats={this.props.allChats} addAChat={this.addAChat} thisChat={this.props.thisChat} thisChatMessages={this.props.thisChatMessages} onChatSubmit={this.props.onChatSubmit} message={this.props.message} onMessageChange={this.props.onMessageChange}/>
+                {this.state.showTopButton ? <button id="scroll-button" title="Go to top" onClick={this.scrollToTop}>Top</button> : ""}
             </div>
         )
     }
