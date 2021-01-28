2c13a8cbff0c1e310cc857522fa7bf2ff1ad179d
EthanJF <ethanjosephfournier@gmail.com>
2021-01-28 14:41:05 -0500

    changed recommended friends list to 6

--- src/components/AreaUsers.js
+++ src/components/AreaUsers.js
@@ -7,7 +7,7 @@ export default class AreaUsers extends Component {
     render(){
     
         let rand = this.props.randomNumber
-        const allUsers = this.props.nearbyUsers.slice(rand,(rand + 12)).map((user) => {
+        const allUsers = this.props.nearbyUsers.slice(rand,(rand + 6)).map((user) => {
             return <UserCard user={user} key={uuid()} setID={this.props.setID}/>
         })
         return(
